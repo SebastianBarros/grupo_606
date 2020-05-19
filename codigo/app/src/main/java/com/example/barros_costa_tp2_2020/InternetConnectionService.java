a920a36636fbccd70a60c418ebc03395c3895111
Seba <sebastian.barros@hotmail.com>
2020-05-19 00:46:39 -0300

    small change

--- codigo/app/src/main/java/com/example/barros_costa_tp2_2020/InternetConnectionService.java
+++ codigo/app/src/main/java/com/example/barros_costa_tp2_2020/InternetConnectionService.java
@@ -11,17 +11,10 @@ public class InternetConnectionService {
     //Static method to check if the user is currently connected to a network. It doesn't matter if it's mobile data or wifi.
 
     public static boolean thereIsInternetConnection(Context context) {
-        boolean connected;
         ConnectivityManager connectivityManager;
         connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-            //we are connected to a network
-            connected = true;
-        } else {
-            connected = false;
-        }
-        return connected;
+        return connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED;
     }
 
 }
