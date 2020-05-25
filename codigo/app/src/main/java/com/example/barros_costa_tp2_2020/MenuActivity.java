9cc3ee8d5c7716bd32c4a4eca15849e5396f6129
costalautaro <costalauta@gmail.com>
2020-05-25 17:05:25 -0300

    fix conflicts rest

--- codigo/app/src/main/java/com/example/barros_costa_tp2_2020/MenuActivity.java
+++ codigo/app/src/main/java/com/example/barros_costa_tp2_2020/MenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.barros_costa_tp2_2020;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -15,8 +16,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
@@ -27,18 +30,14 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
-public class MenuActivity extends AppCompatActivity implements SensorEventListener {
-
-    private static final String URI_EVENT = "http://so-unlam.net.ar/api/api/event";
-    private static final String EVENT_ACTION = "com.example.barros_costa_tp2_2020.intent.action.RESPONSE_EVENT";
-
-
 
 public class MenuActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String PROXIMITY = "PROXIMITY";
     private static final String LIGHT = "LIGHT";
     private static final int MAX_LENGTH = 3;
+    private static final String URI_EVENT = "http://so-unlam.net.ar/api/api/event";
+    private static final String EVENT_ACTION = "com.example.barros_costa_tp2_2020.intent.action.RESPONSE_EVENT";
 
     // variables de managment de sensores
     private SensorManager sensorManagerLux;
@@ -58,7 +57,6 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
     private ArrayList<Float> stackluxValue = new ArrayList<Float>();
     ReceptorServiceEvent receiver;// variables tipo float para almacenar los valores captados por el sensor
 
-
     private List<Float> lightValues = new ArrayList<>();
     private List<Float> proximityValues = new ArrayList<>();
 
@@ -72,9 +70,9 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         LinearLayout lightLayout = findViewById(R.id.linearLayoutLux);
-        LinearLayout  proximityLayout = findViewById(R.id.linearLayoutProximity);
+        LinearLayout proximityLayout = findViewById(R.id.linearLayoutProximity);
         Button saveLightData = findViewById(R.id.buttonRegisterLuxSensor);
-        Button saveProximityData =  findViewById(R.id.buttonRegisterProximitySensor);
+        Button saveProximityData = findViewById(R.id.buttonRegisterProximitySensor);
         Button goBackToLogin = findViewById(R.id.buttonBack);
 
 
@@ -116,37 +114,34 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
         //disparar 2 hilos que manejen el sensor de luz y de proximidad
 
 
+    }
 
-        }
+    protected void onResume() {
+        super.onResume();
+        //los sensores no haran nada hasta que la actividad este en primer plano.
+        // aca se registran, no olvidar de-registrarlos porque en si se la actividad va onPaused y no lo haces siguen corriendo.
+        //tengo que ver como meter esto en dos hilos distintos. por ahora no llego a colgarse pero deberia ir en cada hilo tengo entendido
+        sensorManagerLux.registerListener(this, lux, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManagerProximity.registerListener(this, proximity, SensorManager.SENSOR_DELAY_NORMAL);
 
-        protected void onResume()
-        {
-            super.onResume();
-            //los sensores no haran nada hasta que la actividad este en primer plano.
-            // aca se registran, no olvidar de-registrarlos porque en si se la actividad va onPaused y no lo haces siguen corriendo.
-            //tengo que ver como meter esto en dos hilos distintos. por ahora no llego a colgarse pero deberia ir en cada hilo tengo entendido
-            sensorManagerLux.registerListener(this,lux,SensorManager.SENSOR_DELAY_NORMAL);
-            sensorManagerProximity.registerListener(this,proximity,SensorManager.SENSOR_DELAY_NORMAL);
+        //Inicias el sharedPref y creas un editor
+        sharedPref = MenuActivity.this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        //Para levantar los datos usas getFloat. El primer parametro es la key (0 es el valor mas nuevo, 2 el mas viejo) y el 2do parametro es x mi no encuentra nada, devuelve ese nro
+        Log.i("TEST", String.valueOf(sharedPref.getFloat("PROXIMITY0", 404)));
+        Log.i("TEST", String.valueOf(sharedPref.getFloat("PROXIMITY1", 404)));
+        Log.i("TEST", String.valueOf(sharedPref.getFloat("PROXIMITY2", 404)));
+        Log.i("TEST", String.valueOf(sharedPref.getFloat("LIGHT0", 404)));
+        Log.i("TEST", String.valueOf(sharedPref.getFloat("LIGHT1", 404)));
+        Log.i("TEST", String.valueOf(sharedPref.getFloat("LIGHT2", 404)));
 
-            //Inicias el sharedPref y creas un editor
-            sharedPref = MenuActivity.this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-            editor = sharedPref.edit();
-            //Para levantar los datos usas getFloat. El primer parametro es la key (0 es el valor mas nuevo, 2 el mas viejo) y el 2do parametro es x mi no encuentra nada, devuelve ese nro
-            Log.i("TEST", String.valueOf(sharedPref.getFloat("PROXIMITY0", 404)));
-            Log.i("TEST", String.valueOf(sharedPref.getFloat("PROXIMITY1", 404)));
-            Log.i("TEST", String.valueOf(sharedPref.getFloat("PROXIMITY2", 404)));
-            Log.i("TEST", String.valueOf(sharedPref.getFloat("LIGHT0", 404)));
-            Log.i("TEST", String.valueOf(sharedPref.getFloat("LIGHT1", 404)));
-            Log.i("TEST", String.valueOf(sharedPref.getFloat("LIGHT2", 404)));
+    }
 
-        }
-
-        protected void onPause()
-        {
-            super.onPause();
-            sensorManagerLux.unregisterListener(this);
-            sensorManagerProximity.unregisterListener(this);
-        }
+    protected void onPause() {
+        super.onPause();
+        sensorManagerLux.unregisterListener(this);
+        sensorManagerProximity.unregisterListener(this);
+    }
 
     private void goBackToLogin() {
         MenuActivity.this.startActivity(new Intent(MenuActivity.this, MainActivity.class));
@@ -154,41 +149,46 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
 
     private void saveProximityData() throws JSONException {
         jsonObject = new JSONObject();
-        jsonObject.put("env","TEST");
-        jsonObject.put("type_events","Proximity Sensor");
-        jsonObject.put("state","ACTIVO");
-        jsonObject.put("description",String.valueOf(proximityValue));
+        jsonObject.put("env", "TEST");
+        jsonObject.put("type_events", "Proximity Sensor");
+        jsonObject.put("state", "ACTIVO");
+        jsonObject.put("description", String.valueOf(proximityValue));
 
         Intent intentEvent = new Intent(MenuActivity.this, ServicePostEvent.class);
-        intentEvent.putExtra("uri",URI_EVENT);
-        intentEvent.putExtra("token",token);
-        intentEvent.putExtra("jsondata",jsonObject.toString());
-        intentEvent.putExtra("action",EVENT_ACTION);
+        intentEvent.putExtra("uri", URI_EVENT);
+        intentEvent.putExtra("token", token);
+        intentEvent.putExtra("jsondata", jsonObject.toString());
+        intentEvent.putExtra("action", EVENT_ACTION);
         configureBroadCastReceiver();
         startService(intentEvent);
     }
 
     private void configureBroadCastReceiver() {
-      receiver = new ReceptorServiceEvent();
-        IntentFilter filterRegister =  new IntentFilter(EVENT_ACTION);
+        receiver = new ReceptorServiceEvent();
+        IntentFilter filterRegister = new IntentFilter(EVENT_ACTION);
         filterRegister.addCategory(Intent.CATEGORY_DEFAULT);
         registerReceiver(receiver, filterRegister);
     }
 
     private void saveLightData() throws JSONException {
         jsonObject = new JSONObject();
-        jsonObject.put("env","TEST");
-        jsonObject.put("type_events","Lux Sensor");
-        jsonObject.put("state","ACTIVO");
-        jsonObject.put("description",String.valueOf(luxValue));
+        jsonObject.put("env", "TEST");
+        jsonObject.put("type_events", "Lux Sensor");
+        jsonObject.put("state", "ACTIVO");
+        jsonObject.put("description", String.valueOf(luxValue));
 
         Intent intentEvent = new Intent(MenuActivity.this, ServicePostEvent.class);
-        intentEvent.putExtra("uri",URI_EVENT);
-        intentEvent.putExtra("token",token);
-        intentEvent.putExtra("jsondata",jsonObject.toString());
-        intentEvent.putExtra("action",EVENT_ACTION);
+        intentEvent.putExtra("uri", URI_EVENT);
+        intentEvent.putExtra("token", token);
+        intentEvent.putExtra("jsondata", jsonObject.toString());
+        intentEvent.putExtra("action", EVENT_ACTION);
         configureBroadCastReceiver();
         startService(intentEvent);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     @Override
@@ -197,87 +197,81 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
         // referencio los textView del layout.
         // esto solo muestra en pantalla
 
-
-
         //Esta lógica es para diferenciar los eventos del sensor .getType me da el tipo de sensor que mensajeo a event.sensor
-<<<<<<< HEAD
-        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY)
-        {
+
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             proximityValue = event.values[0];
             Log.i("valor proximidad:", String.valueOf(proximityValue));
             textViewProximity.setText(Float.toString(proximityValue));  //seteo el ultimo valor que cambio de proximidad
         }
-        if (event.sensor.getType() == Sensor.TYPE_LIGHT)
-        {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
             //luxValue = event.values[0];
 
             textViewLux.setText(Float.toString(luxValue)); // seteo el ultimo valor que cambio el nivel de luz
-=======
-        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
-            proximity = event.values[0];
-            Log.i("valor proximidad:", String.valueOf(proximity) + event.timestamp);
+
+            if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                proximityValue = event.values[0];
+                Log.i("valor proximidad:", String.valueOf(proximity) + event.timestamp);
 
 
-            //la primera vez que se ejecute (y no este el sharedPreferences, el array va a estar vacio. Vamos a guardar los primeros 3 valores.
-            // Dsp los vamos actualizando a medida que cambien. En 0 esta el valor mas actual, en 2 esta el mas viejo.
-            // Cada vez que hago un add en la pos 0, desplaza los demas, y dsp elimino la pos 3 para mantener q sea de 3 posiciones.
-            if(proximityValues.size() < MAX_LENGTH) {
-                proximityValues.add(proximity);
+                //la primera vez que se ejecute (y no este el sharedPreferences, el array va a estar vacio. Vamos a guardar los primeros 3 valores.
+                // Dsp los vamos actualizando a medida que cambien. En 0 esta el valor mas actual, en 2 esta el mas viejo.
+                // Cada vez que hago un add en la pos 0, desplaza los demas, y dsp elimino la pos 3 para mantener q sea de 3 posiciones.
+                if (proximityValues.size() < MAX_LENGTH) {
+                    proximityValues.add(proximityValue);
+                }
+
+                if (proximityValue != proximityValues.get(0)) {
+                    proximityValues.add(0, proximityValue);
+                    proximityValues.remove(MAX_LENGTH);
+                    update(PROXIMITY, proximityValues);
+                }
+                //acá llamarías al método para mostrar en pantalla, en el vector tenés los valores
+
+                textViewProximity.setText(Float.toString(proximityValue));  //seteo el ultimo valor que cambio de proximidad
+
             }
 
-            if(proximity != proximityValues.get(0)){
-                proximityValues.add(0, proximity);
-                proximityValues.remove(MAX_LENGTH);
-                update(PROXIMITY, proximityValues);
-            }
-            //acá llamarías al método para mostrar en pantalla, en el vector tenés los valores
+            if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+                luxValue = event.values[0];
+                Log.i("valor luz:", String.valueOf(lux));
 
-            textViewProximity.setText(Float.toString(proximity));  //seteo el ultimo valor que cambio de proximidad
+
+                //la primera vez que se ejecute (y no este el sharedPreferences, el array va a estar vacio. Vamos a guardar los primeros 3 valores.
+                // Dsp los vamos actualizando a medida que cambien. El 3 es un valor aleatorio que elegi. Si querés mostrar 10 valores en pantalla, entonces acá poner un 10.
+                if (lightValues.size() < MAX_LENGTH) {
+                    lightValues.add(luxValue);
+                }
+
+                if (luxValue != lightValues.get(0) && Math.abs(event.values[0] - lightValues.get(0)) > 50) {
+                    lightValues.add(0, luxValue);
+                    lightValues.remove(MAX_LENGTH);
+                    update(LIGHT, lightValues);
+                }
+
+                //acá llamarías al método para mostrar en pantalla, en el vector tenés los valores
+                textViewLux.setText(Float.toString(luxValue)); // seteo el ultimo valor que cambio el nivel de luz
+            }
 
         }
 
-        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
-            lux = event.values[0];
-            Log.i("valor luz:", String.valueOf(lux));
+        //Cada vez que varia el valor de los sensores, guardo en el SharedPreferences
 
 
-            //la primera vez que se ejecute (y no este el sharedPreferences, el array va a estar vacio. Vamos a guardar los primeros 3 valores.
-            // Dsp los vamos actualizando a medida que cambien. El 3 es un valor aleatorio que elegi. Si querés mostrar 10 valores en pantalla, entonces acá poner un 10.
-            if(lightValues.size() < MAX_LENGTH) {
-                lightValues.add(lux);
-            }
-
-            if(lux != lightValues.get(0) && Math.abs(event.values[0] - lightValues.get(0)) > 50) {
-                lightValues.add(0, lux);
-                lightValues.remove(MAX_LENGTH);
-                update(LIGHT, lightValues);
-            }
-
-            //acá llamarías al método para mostrar en pantalla, en el vector tenés los valores
-            textViewLux.setText(Float.toString(lux)); // seteo el ultimo valor que cambio el nivel de luz
-        }
+        //obtener el nombre del user
+        //botón logout
+        //botón registrar -> creás un hilo aparte donde levantás los datos de la pantalla y hacés 2 post con diferente type y datos.
+        //actualizar las grillas con los datos de los sensores
 
     }
 
-    //Cada vez que varia el valor de los sensores, guardo en el SharedPreferences
     private void update(String type, List<Float> values) {
 
-        for(int i = 0; i < values.size(); i++) {
-            editor.putFloat(type+i, values.get(i));
+
+        for (int i = 0; i < values.size(); i++) {
+            editor.putFloat(type + i, values.get(i));
         }
         editor.apply();
     }
-
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-
-    //obtener el nombre del user
-    //botón logout
-    //botón registrar -> creás un hilo aparte donde levantás los datos de la pantalla y hacés 2 post con diferente type y datos.
-    //actualizar las grillas con los datos de los sensores
 
 }
